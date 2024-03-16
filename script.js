d12ec38c09c2a522d90cccf29aeea260057beb71
eijikong21 <eijinakayama21@gmail.com>
2024-03-16 15:24:22 +0800

    just needs to a vertical win condition

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Credits to Alex Younger for the code check him at https://www.ayweb.dev/blog/building-a-house-from-the-inside-out
 function Gameboard(){
     //modified the code from connect 4 6x7 arrays into 3x3 for tictactoe 
-    
     const rows=3;
     const columns=3;
     const board =[];
@@ -12,8 +11,14 @@ function Gameboard(){
         }
       }
     const getBoard = () => board;
+     // Getter function for the win variable
+    
+
   
+    
     const pickBox = (column,row, player) => {
+     
+    
       //makes sure that token wont overide each other and turn wont be used if it ever happens
      if(board[row][column].getValue()!==0){
          console.log("Please try again space is already allocated");
@@ -21,54 +26,47 @@ function Gameboard(){
          return; 
        } 
       board[row][column].addToken(player);
-      let a=game.getActivePlayer().token;
-       let win=0;
-     console.log(a);
-      //vertical win condition
-      const winColChk = board.filter((row) => row[column].getValue() === game.getActivePlayer().token).map(row => row[column]);
-      if (winColChk.length==3){
-        console.log(`${game.getActivePlayer().name} Wins`);
-        win=1;
-        return;
-      }
-
-// horizontal win condition
-    for(let i=0;i<3;i++){
-        const winRowChk=board[i].map((cell)=> cell.getValue())
-      if (JSON.stringify(winRowChk) === JSON.stringify([a,a,a])) {
-        console.log(`${game.getActivePlayer().name} Wins`);
-        win=1;
-        return;
-}
-    }
-    const chkBoard=board.map((row)=>row.map(cell=>cell.getValue()))
-    //diagonal win condition
-    if ((chkBoard[0][0] === a && chkBoard[1][1] === a && chkBoard[2][2] === a) || (chkBoard[0][2] === a && chkBoard[1][1] === a && chkBoard[2][0] === a)){
-      console.log(`${game.getActivePlayer().name} Wins`);
-      win=1;
-    }
-   //tie condition
-    const hasDefaultValue = chkBoard.some(row => row.includes(0));
-    console.log(hasDefaultValue);
-    if(hasDefaultValue==false && win==0){
-      console.log('its a tie');
-    }
+    
+//       let a=game.getActivePlayer().token;
+//       //vertical win condition
+//       const winColChk = board.filter((row) => row[column].getValue() === game.getActivePlayer().token).map(row => row[column]);
+//       if (winColChk.length==3){
+//         console.log(`${game.getActivePlayer().name} Wins`);
+//         win=1;
+//       }
+// // horizontal win condition
+//     for(let i=0;i<3;i++){
+      
+//         const winRowChk=board[i].map((cell)=> cell.getValue())
+//       if (JSON.stringify(winRowChk) === JSON.stringify([a,a,a])) {
+//         console.log(`${game.getActivePlayer().name} Wins`);
+//         win=1;
+// }
+//     }
+//     const chkBoard=board.map((row)=>row.map(cell=>cell.getValue()))
+//     //diagonal win condition
+//     if ((chkBoard[0][0] === a && chkBoard[1][1] === a && chkBoard[2][2] === a) || (chkBoard[0][2] === a && chkBoard[1][1] === a && chkBoard[2][0] === a)){
+//       console.log(`${game.getActivePlayer().name} Wins`);
+//       win=1;
+//       gameResult.textContent=`${game.getActivePlayer().name} Wins`;
+//     }
+//    //tie condition
+//     const hasDefaultValue = chkBoard.some(row => row.includes(0));
+//     if(hasDefaultValue==false && win==0){
+//       console.log('its a tie');
+//     }
     };
       const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-       
-            
         console.log(boardWithCellValues);
-       
-       
       };
-      return { getBoard, pickBox, printBoard,};
+      return { getBoard, pickBox, printBoard};
     }
    
 
 function Cell() {
     let value = 0;
-  
+    
    
     const addToken = (player) => {
       value = player;
@@ -113,45 +111,93 @@ function Cell() {
     };
   
     const playRound = (column,row) => {
-    
       console.log(
         `Dropping ${getActivePlayer().name}'s token into column ${column}... row ${row}...`
       );
       board.pickBox(column,row, getActivePlayer().token);
+      endGame();
+     
+      
      // console.log(board.getBoard());
       //console.log(board.getBoard().map((row)=>row.map((cell)=>cell.getValue())));
       switchPlayerTurn();
       printNewRound();
      
     };
-  
-   
+   let win=0;
+   const endGame=()=>{
+    const playerTurnDiv = document.querySelector('.result');
+    a=getActivePlayer().token;
+    const chkBoard = board.getBoard().map((row)=>row.map((cell)=>cell.getValue()));
+    //diagonal win condition
+if ((chkBoard[0][0] === a && chkBoard[1][1] === a && chkBoard[2][2] === a) || (chkBoard[0][2] === a && chkBoard[1][1] === a && chkBoard[2][0] === a)){
+  playerTurnDiv.textContent = `${activePlayer.name} Wins`;
+console.log(`${game.getActivePlayer().name} Wins`);
+win=1;
+}
+     //tie condition
+    const hasDefaultValue = chkBoard.some(row => row.includes(0));
+    if(hasDefaultValue==false && win==0){
+      playerTurnDiv.textContent = `Its a Tie!`;
+      console.log('its a tie');
+      win=1;
+    }
+    // horizontal win condition
+    for(let i=0;i<3;i++){
+        const winRowChk=board.getBoard()[i].map((cell)=> cell.getValue())
+      if (JSON.stringify(winRowChk) === JSON.stringify([a,a,a])) {
+        playerTurnDiv.textContent = `${activePlayer.name} Wins`;
+        console.log(`${game.getActivePlayer().name} Wins`);
+        win=1;
+      }
+}
+for(let i=0;i<3;i++){
+  for(let j=0;j<3;j++){
+  const winRowChk=board.getBoard().map((row)=>row.map((cell)=>cell.getValue()));
+  console.log(winRowChk[j][i]);
+if (JSON.stringify(winRowChk[j][i]) === JSON.stringify([a])) {
+  playerTurnDiv.textContent = `${activePlayer.name} Wins`;
+  console.log(`${game.getActivePlayer().name} Wins`);
+  win=1;
+}
+}
+}
+   }
+   const getWin=()=>win;
     printNewRound();
     return {
       playRound,
       getActivePlayer,
       switchPlayerTurn,
+      getWin,
       getBoard: board.getBoard
     };
   }
   const game = GameController();
   function ScreenController() {
-    
+   
     const playerTurnDiv = document.querySelector('.turn');
     const boardDiv = document.querySelector('.board');
-  
+    
     const updateScreen = () => {
       // clear the board
       boardDiv.textContent = "";
   
       // get the newest version of the board and player turn
-
+      
       const board = game.getBoard();
       const activePlayer = game.getActivePlayer();
- 
-      // Display player's turn
-      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+      
   
+      if(game.getWin()==1){
+       
+        playerTurnDiv.textContent =``;
+    
+      }
+      // Display player's turn
+      else{
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+      }
       // Render board squares
       board.forEach((row,rowIndex) => {
         row.forEach((cell, columnIndex) => {
@@ -172,20 +218,26 @@ function Cell() {
           boardDiv.appendChild(cellButton);
         })
       })
+     
     }
-  
+    
+
     // Add event listener for the board
     function clickHandlerBoard(e) {
+      if(game.getWin()==1)return;
       const selectedColumn = e.target.dataset.column;
       const selectedRow = e.target.dataset.row;
       // console.log(selectedRow);  
       // console.log(selectedColumn);  
       // // Make sure I've clicked a column and not the gaps in between
        if (!selectedColumn || !selectedRow) return;
-      
+       
       game.playRound(selectedColumn,selectedRow);
+     
       updateScreen();
     }
+  
+
     boardDiv.addEventListener("click", clickHandlerBoard);
   
     // Initial render
